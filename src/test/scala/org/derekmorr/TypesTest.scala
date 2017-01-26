885b2d870e2f36f40643715d36be2daa80049cf8
Derek Morr <derekmorr@psu.edu>
2017-01-26 09:05:50 -0500

    reindent w/ 2 spaces

--- src/test/scala/org/derekmorr/TypesTest.scala
+++ src/test/scala/org/derekmorr/TypesTest.scala
@@ -1,46 +1,43 @@
 package org.derekmorr
 
-import eu.timepit.refined.auto._
-import org.derekmorr.types.util.{dns, ip, safe, uint}
-
 class TypesTest extends BaseTest {
 
 
-    "Not compile invalid types" when {
-        "it's an IPv4 address" in {
-            """val noOk = ip("999.999.999.999")""" mustNot compile
-        }
-
-        "it's an IPv6 address" in {
-            """val nope = ip("2001:db8::hello")""" mustNot compile
-        }
-
-        "it's DNS" in {
-            """val noDns = dns("jjjj.,<>")""" mustNot compile
-        }
-
-        "it's an unsigned integer" in {
-            """val noInt = uint("hello")""" mustNot compile
-        }
+  "Not compile invalid types" when {
+    "it's an IPv4 address" in {
+      """val noOk = ip("999.999.999.999")""" mustNot compile
     }
 
-    "Compile valid types" when {
-        "it's an IPv4 address" in {
-            """val ipv4 = ip("192.168.0.1")""" must compile
-        }
-
-        "it's an IPv6 address" in {
-            """val ipv6 = ip("2001:db8::abcd:123")""" must compile
-        }
-
-        "it's a DNS name" in {
-            """val hostname = dns("foo.bar.org.blah")""" must compile
-        }
-
-        "it's an unsigned integer" in {
-            // maxint + 1
-            """val j = uint("2147483648")""" must compile
-        }
+    "it's an IPv6 address" in {
+      """val nope = ip("2001:db8::hello")""" mustNot compile
     }
+
+    "it's DNS" in {
+      """val noDns = dns("jjjj.,<>")""" mustNot compile
+    }
+
+    "it's an unsigned integer" in {
+      """val noInt = uint("hello")""" mustNot compile
+    }
+  }
+
+  "Compile valid types" when {
+    "it's an IPv4 address" in {
+      """val ipv4 = ip("192.168.0.1")""" must compile
+    }
+
+    "it's an IPv6 address" in {
+      """val ipv6 = ip("2001:db8::abcd:123")""" must compile
+    }
+
+    "it's a DNS name" in {
+      """val hostname = dns("foo.bar.org.blah")""" must compile
+    }
+
+    "it's an unsigned integer" in {
+      // maxint + 1
+      """val j = uint("2147483648")""" must compile
+    }
+  }
 
 }
